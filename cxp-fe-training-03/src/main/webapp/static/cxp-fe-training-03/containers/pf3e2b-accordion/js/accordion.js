4fc916653ef80456ae13bcd03153daeecb01e305
Stéphane P. Péricat <stephane@backbase.com>
2015-03-11 09:52:05 +0100

    fix indent

--- cxp-fe-training-03/src/main/webapp/static/cxp-fe-training-03/containers/pf3e2b-accordion/js/accordion.js
+++ cxp-fe-training-03/src/main/webapp/static/cxp-fe-training-03/containers/pf3e2b-accordion/js/accordion.js
@@ -1,41 +1,41 @@
 (function() {
 
-	var SELECTORS = {
-		SECTION_HEADER: '[data-js=bt-accordion-section-header]',
-		SECTION_BODY: '[data-js=bt-accordion-section-body]'
-	};
+    var SELECTORS = {
+        SECTION_HEADER: '[data-js=bt-accordion-section-header]',
+        SECTION_BODY: '[data-js=bt-accordion-section-body]'
+    };
 
-	var CLASSES = {
-		HIDDEN: "hidden"
-	};
+    var CLASSES = {
+        HIDDEN: "hidden"
+    };
 
-	var Container = b$.bdom.getNamespace('http://backbase.com/2013/portalView').getClass('container');
+    var Container = b$.bdom.getNamespace('http://backbase.com/2013/portalView').getClass('container');
 
-	Container.extend(function(bDomDocument, node) {
-		Container.apply(this, arguments);
-		this.isPossibleDragTarget = true;
-	}, {
-		localName: 'AccordionPF3E2b',
-		namespaceURI: 'cxp-fe-training-03'
-	}, {
-		handlers: {
-			DOMReady: function(event) {
-				var $container = $(this.htmlNode),
-				    $sections = $container.find(SELECTORS.SECTION_BODY);
+    Container.extend(function(bDomDocument, node) {
+        Container.apply(this, arguments);
+        this.isPossibleDragTarget = true;
+    }, {
+        localName: 'AccordionPF3E2b',
+        namespaceURI: 'cxp-fe-training-03'
+    }, {
+        handlers: {
+            DOMReady: function(event) {
+                var $container = $(this.htmlNode),
+                    $sections = $container.find(SELECTORS.SECTION_BODY);
 
-				$container.on('click', SELECTORS.SECTION_HEADER, function(event) {
-					$sections.addClass(CLASSES.HIDDEN);
-					$(this).parent().children(SELECTORS.SECTION_BODY).removeClass(CLASSES.HIDDEN);
-				});
-			},
-			preferencesSaved: function(event) {
-				if (event.target.model.uuid == event.currentTarget.model.uuid) {
-					event.currentTarget.refreshHTML();
-				}
-			},
-			dragDrop: function(event) {
-				console.log(event);
-			}
-		}
-	});
+                $container.on('click', SELECTORS.SECTION_HEADER, function(event) {
+                    $sections.addClass(CLASSES.HIDDEN);
+                    $(this).parent().children(SELECTORS.SECTION_BODY).removeClass(CLASSES.HIDDEN);
+                });
+            },
+            preferencesSaved: function(event) {
+                if (event.target.model.uuid == event.currentTarget.model.uuid) {
+                    event.currentTarget.refreshHTML();
+                }
+            },
+            dragDrop: function(event) {
+                console.log(event);
+            }
+        }
+    });
 })();
